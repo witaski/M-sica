16af4a7f676cad6ef177689d2997ddb4b77ea56f
Wellington Witaski <wellingtonwitaski@hotmail.com>
2021-08-23 16:28:17 -0300

    Exercício 3

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace M_sica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!!");
+        }
+    }
+}
