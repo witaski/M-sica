7c258ae211b3b946bca200cc27f77996928e80dc
Wellington Witaski <wellingtonwitaski@hotmail.com>
2021-08-29 12:51:30 -0300

    Adiciona solução do exercício  M_sica

--- Program.cs
+++ Program.cs
@@ -6,7 +6,70 @@ namespace M_sica
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!!");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("A Ele a Glória - Diante do trono");
+            Console.ResetColor();
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Porque Dele e por Ele");
+            Console.WriteLine("Para Ele são todas as coisas");
+            Console.WriteLine("Porque Dele e por Ele");
+            Console.WriteLine("Para Ele são todas as coisas");
+            Console.WriteLine();
+
+            Console.WriteLine("Porque Dele e por Ele");
+            Console.WriteLine("Para Ele são todas as coisas");
+            Console.WriteLine("Porque Dele e por Ele");
+            Console.WriteLine("Para Ele são todas as coisas");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine("A Ele a glória");
+            Console.WriteLine("A Ele a glória");
+            Console.WriteLine("A Ele a glória");
+            Console.WriteLine("Pra sempre");
+            Console.WriteLine("Amém");
+            Console.WriteLine();
+
+
+
+            Console.WriteLine("A Ele a glória");
+            Console.WriteLine("A Ele a glória");
+            Console.WriteLine("A Ele a glória");
+            Console.WriteLine("Pra sempre");
+            Console.WriteLine("Amém");
+            Console.WriteLine();
+            Console.ResetColor();
+
+            Console.WriteLine("Quão profundas riquezas");
+            Console.WriteLine("O saber e o conhecer de Deus");
+            Console.WriteLine("Quão insondáveis");
+            Console.WriteLine("Seus juízos e Seus caminhos");
+            Console.WriteLine();
+
+            Console.WriteLine("Quão profundas riquezas");
+            Console.WriteLine("O saber e o conhecer de Deus");
+            Console.WriteLine("Quão insondáveis");
+            Console.WriteLine("Seus juízos e Seus caminhos");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine("A Ele a glória");
+            Console.WriteLine("A Ele a glória");
+            Console.WriteLine("A Ele a glória");
+            Console.WriteLine("Pra sempre");
+            Console.WriteLine("Amém");
+            Console.WriteLine();
+
+            Console.WriteLine("A Ele a glória");
+            Console.WriteLine("A Ele a glória");
+            Console.WriteLine("A Ele a glória");
+            Console.WriteLine("Pra sempre");
+            Console.WriteLine("Amém");
+            Console.ResetColor();
+
         }
     }
 }
